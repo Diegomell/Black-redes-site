0a36cad5ad9ca238ea3b88eefffbfb65a0bd7b8d
Diegomell <143726829+Diegomell@users.noreply.github.com>
2024-02-03 21:18:26 -0300

    Delete codigos.js

--- codigos.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav");
-const opcao = document.querySelector(".opcao");
-
-hamburger.addEventListener("click", () => nav.classList.toggle("active"));
-opcao.addEventListener("click", () => nav.classList.remove("active"));
